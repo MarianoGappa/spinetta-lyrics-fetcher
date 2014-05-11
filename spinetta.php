86d13d187a2c6cd29f4039838eb91fbe0cdf46aa
Mariano Gappa <mariano@movio.co>
2014-05-11 21:04:27 +1200

    License notice + removed var_dump.

--- spinetta.php
+++ spinetta.php
@@ -1,3 +1,21 @@
+/************************************************************************************************
+This file is part of spinetta-lyrics-fetcher.
+ 
+spinetta-lyrics-fetcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+spinetta-lyrics-fetcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with spinetta-lyrics-fetcher.  If not, see <http://www.gnu.org/licenses/>.
+eral Public License along with spinetta-lyrics-fetcher. If not, see http://www.gnu.org/licenses/.
+************************************************************************************************/
+
 <?php
 date_default_timezone_set("Pacific/Auckland");
 
@@ -16,7 +34,6 @@ foreach($artists as $artist)
 function getAllAlbumsFormArtist() {
     $html = file_get_contents("http://www.rock.com.ar/artistas/luis-alberto-spinetta");
     preg_match_all("#<a href=\"(/discos/[\d]+/[\d]+\.shtml)#", $html, $matches, PREG_SET_ORDER);
-    var_dump($matches);
     foreach($matches as $match) {
         getAllSongsFromAlbum($match[1]);
     }    
